851e9185a6df0c9ef4891f2ca1d28705bc056cb9
Andrii Kuvardin <psy.akg@gmail.com>
2026-05-20 09:17:47 +0200

    Add Product class and display product list

--- SchuhShop/Program.cs
+++ SchuhShop/Program.cs
@@ -1,15 +1,27 @@
-﻿namespace SchuhShop
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchuhShop
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Willkommen im Schuh Shop!");
-            
-            Product product = new Product();
-            Cart cart = new Cart();
-            CartItem cartItem = new CartItem();
-            ShopService shopService = new ShopService();
+            Console.WriteLine("Willkommen im Schuh Shop!\n");
+
+            List<Product> products = new List<Product>
+            {
+                new Product(1, "Nike Air Max", 120),
+                new Product(2, "Adidas Superstar", 90),
+                new Product(3, "Puma Runner", 80)
+            };
+
+            Console.WriteLine("Unsere Produkte:");
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"{product.Id}. {product.Name} - {product.Price} Euro");
+            }
         }
     }
 }
